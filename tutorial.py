a2a4b74b9209f08a1cb81c3fe1fb6afc69a37a1f
keimir14 <keimerasprilla@gmail.com>
2023-06-13 18:21:30 -0500

    COMMIT INICIAL

---
+++ tutorial.py
@@ -0,0 +1,23 @@
+mensaje = "hola"
+espacio = " "
+nombre = "keimir" 
+print(mensaje + espacio + nombre)
+
+numero_uno = 4
+numero_dos = 6
+resultado = numero_uno + numero_dos
+resultado = str(resultado)
+print("el resultado de la suma es:" + resultado)
+
+print("busqueda")
+mensaje = "hola keimir"
+buscar_subcadena = mensaje.find("keimir")
+print (buscar_subcadena)
+
+mensaje = "hola keimir"
+extraer_subcadena = mensaje[5:11]
+print(extraer_subcadena)
+
+mensaje_uno = "hola"
+mensaje_dos = "hola"
+print (mensaje_uno == mensaje_dos)
